3a1e034ffb34d7a43b3e0b57cbcebae424a91c13
Chaejun Lee <chaejunlee@knu.ac.kr>
2024-01-31 16:07:25 -0800

    chore: re-init

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMeta {
-  env: {
-      VITE_PORT?: string,
-  }
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
-import { defineConfig,loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default ({mode}) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  let port : number | string = process.env.VITE_PORT;
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  let port: number | string = process.env.VITE_PORT;
   if (!port || isNaN(Number(port))) {
-    throw new Error('.env.local 파일에서 올바른 "VITE_PORT" 값을 설정해야 합니다.');
+    throw new Error(
+      '.env.local 파일에서 올바른 "VITE_PORT" 값을 설정해야 합니다.'
+    );
   }
-  port = parseInt(port,10)
+  port = parseInt(port, 10);
   return defineConfig({
     plugins: [react()],
-    server:{
-      port
-    }
-  })
-}
+    server: {
+      port,
+    },
+  });
+};
