acdc62482d00435b0678e735ae490295e157ae15
toothlessdev <toothless042@gmail.com>
2024-03-17 18:25:18 +0900

    Init

--- storybook-static/assets/preview-exU0BvKL.js
+++
@@ -1,7 +0,0 @@
-import{_ as e}from"./iframe-LxV13yTg.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-jQNUBQcf.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10]),import.meta.url);return new r}}};export{a as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-jQNUBQcf.js","./iframe-LxV13yTg.js","./index-XiNr8FW2.js","./_commonjsHelpers-5-cIlDoe.js","./react-18-sFOusbzT.js","./index-9vG4XYWr.js","./index-edjqR4ry.js","./index-ogXoivrg.js","./extends-dGVwEr9R.js","./index-mLPG47JP.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
