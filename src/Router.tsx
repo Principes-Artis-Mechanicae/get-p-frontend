36d329bd802e74ad5854fb359984cdb2853a813c
toothlessdev <toothless042@gmail.com>
2024-03-17 21:43:49 +0900

    chore: 디렉토리 구조 설정

---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
