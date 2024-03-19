dee54b660fd7fd368c8a1d2cc7d5f81573b91b7b
toothlessdev <toothless042@gmail.com>
2024-03-19 23:54:16 +0900

    style: 유틸리티 스타일 추상화 및 분리

---
+++ src/styles/utils.ts
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+
+export const place_center = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const horizontal_center = css`
+    display: flex;
+    justify-content: center;
+`;
+
+export const vertical_center = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
