7b91ce29ef6896bd09008115e9d84c6671678a82
toothlessdev <toothless042@gmail.com>
2024-03-20 00:00:34 +0900

    feat: navItems 상수 추가

---
+++ src/constants/navItems.ts
@@ -0,0 +1,28 @@
+export interface INavItem {
+    key: number;
+    text: string;
+    to: string;
+}
+
+export const navItems: INavItem[] = [
+    {
+        key: 1,
+        text: "프로젝트 찾기",
+        to: "/",
+    },
+    {
+        key: 2,
+        text: "프로젝트 찾기",
+        to: "/",
+    },
+    {
+        key: 3,
+        text: "프로젝트 찾기",
+        to: "/",
+    },
+    {
+        key: 4,
+        text: "프로젝트 찾기",
+        to: "/",
+    },
+];
