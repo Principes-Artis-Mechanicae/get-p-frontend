3ef100a0284813416d980d764165fa368e5cefc0
zladb <zla_db@naver.com>
2024-03-25 20:15:36 +0900

    GETP-107 chore: MemberTypeCard Storybook 작성

---
+++ src/components/auth/MemberTypeCard.story.ts
@@ -0,0 +1,32 @@
+import { Text } from "@/common/typography/Text";
+
+import { MemberTypeCard } from "./MemberTypeCard";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+    title: "auth/MemberTypeCard",
+    component: MemberTypeCard,
+} satisfies Meta<typeof MemberTypeCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Client: Story = {
+    args: {
+        variant: "client",
+        asset: "",
+        width: "208px",
+        height: "208px",
+        children: "의뢰자 회원",
+    },
+};
+
+export const People: Story = {
+    args: {
+        variant: "people",
+        asset: "",
+        width: "208px",
+        height: "208px",
+        children: "피플 회원",
+    },
+};
