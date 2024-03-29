8846fcadd82d36f05fbcdb37bd76cba61a2fd7b5
toothlessdev <toothless042@gmail.com>
2024-03-29 20:12:28 +0900

    style: Main Layout 에 최대폭 지정

--- src/common/layout/MainLayout.tsx
+++ src/common/layout/MainLayout.tsx
@@ -2,12 +2,20 @@ import { Outlet } from "react-router-dom";
 
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import styled from "@emotion/styled";
+
+export const Main = styled.main`
+    width: min(100%, 1440px);
+    margin: 0px auto;
+`;
 
 export const MainLayout = () => {
     return (
         <>
             <Header height="80px" padding="2rem 4rem" gap="25rem" containerGap="1.25rem" />
-            <Outlet />
+            <Main>
+                <Outlet />
+            </Main>
             <Footer />
         </>
     );
