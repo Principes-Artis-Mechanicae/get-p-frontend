6e909b4974d61b65721295f22dfb73dca5771720
chominju <cmj021002@knu.ac.kr>
2024-03-31 23:11:29 +0900

    feature: 회원가입 페이지 구현

--- src/common/form/CheckBox.story.ts
+++ src/common/form/CheckBox.story.ts
@@ -11,40 +11,32 @@ type Story = StoryObj<typeof meta>;
 
 export const Terms: Story = {
     args: {
-        width: "440px",
-        height: "24px",
-        checkWidth: "20px",
-        checkHeight: "20px",
+        width: "20px",
+        height: "20px",
         children: "GET-P 이용약관 (필수)",
     },
 };
 
 export const Privacy: Story = {
     args: {
-        width: "440px",
-        height: "24px",
-        checkWidth: "20px",
-        checkHeight: "20px",
+        width: "20px",
+        height: "20px",
         children: "개인정보 수집 및 이용 동의 (필수)",
     },
 };
 
 export const Marketing: Story = {
     args: {
-        width: "440px",
-        height: "24px",
-        checkWidth: "20px",
-        checkHeight: "20px",
+        width: "20px",
+        height: "20px",
         children: "마케팅 정보 수신 (선택)",
     },
 };
 
 export const Login: Story = {
     args: {
-        width: "440px",
-        height: "24px",
-        checkWidth: "20px",
-        checkHeight: "20px",
+        width: "20px",
+        height: "20px",
         children: "로그인 유지",
     },
 };
