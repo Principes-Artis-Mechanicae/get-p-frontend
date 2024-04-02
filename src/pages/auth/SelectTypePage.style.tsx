02dad32f23ae459e4d2c34ed2c06d6a3844d6530
zladb <zla_db@naver.com>
2024-04-02 14:04:28 +0900

    GETP-117 feat: 회원 유형 선택 페이지 퍼블리싱

---
+++ src/pages/auth/SelectTypePage.style.tsx
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+
+export const SelectTypePageWrapper = styled.section`
+    width: min(100%, 500px);
+    margin: 0px auto;
+`;
+
+export const SelectTypePageHeader = styled.p`
+    margin: 50px 0px;
+
+    text-align: center;
+`;
+
+export const SelectTypeContainer = styled.div`
+    width: 100%;
+`;
+
+export const SelectTypeItem = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    width: 100%;
+    margin: 30px auto;
+`;
--- src/pages/auth/SelectTypePage.tsx
+++ src/pages/auth/SelectTypePage.tsx
@@ -1,24 +1,72 @@
 import { useDispatch } from "react-redux";
 
 import { Button } from "@/common/form/Button";
+import { Text } from "@/common/typography/Text";
 
+import { MemberTypeCard } from "@/components/auth/MemberTypeCard";
+
+import {
+    SelectTypeContainer,
+    SelectTypeItem,
+    SelectTypePageHeader,
+    SelectTypePageWrapper,
+} from "./SelectTypePage.style";
 import { pageActions } from "@/store/slice/page.slice";
 
 export default function SelectTypePage() {
     const dispatch = useDispatch();
 
     return (
-        <>
-            <Button
-                variant="primary"
-                width="100px"
-                height="40px"
-                onClick={() => {
-                    dispatch(pageActions.nextPage());
-                }}
-            >
-                다음
-            </Button>
-        </>
+        <SelectTypePageWrapper>
+            <SelectTypePageHeader>
+                <Text size="xl" color="point" weight="bold">
+                    회원타입
+                </Text>
+                <Text size="xl" color="primary" weight="bold">
+                    을 선택해주세요
+                </Text>
+                <br />
+                <br />
+                <Text size="m" color="secondary" weight="normal">
+                    회원 타입은 프로젝트를 의뢰하는 '의뢰자 회원'과
+                </Text>
+                <br />
+                <Text size="m" color="secondary" weight="normal">
+                    프로젝트에 참여하는 '피플 회원'으로 나누어 집니다.
+                </Text>
+            </SelectTypePageHeader>
+            <SelectTypeContainer>
+                <SelectTypeItem>
+                    <MemberTypeCard
+                        key="clinet-type"
+                        variant="client"
+                        width="208px"
+                        height="208px"
+                        imgSrc=""
+                        label="의뢰자 회원"
+                    ></MemberTypeCard>
+                    <MemberTypeCard
+                        key="people-type"
+                        variant="people"
+                        width="208px"
+                        height="208px"
+                        imgSrc=""
+                        label="피플 회원"
+                    ></MemberTypeCard>
+                </SelectTypeItem>
+                <SelectTypeItem>
+                    <Button
+                        variant="disabled"
+                        width="100%"
+                        height="54px"
+                        onClick={() => {
+                            dispatch(pageActions.nextPage());
+                        }}
+                    >
+                        완료하기
+                    </Button>
+                </SelectTypeItem>
+            </SelectTypeContainer>
+        </SelectTypePageWrapper>
     );
 }
