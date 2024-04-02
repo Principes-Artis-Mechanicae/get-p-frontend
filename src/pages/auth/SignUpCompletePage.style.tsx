5e19866d386fb6d167c55f3610d4088ae70d0bcf
zladb <zla_db@naver.com>
2024-04-02 15:42:33 +0900

    GETP-117 feat: 회원 가입 완료 페이지 퍼블리싱

---
+++ src/pages/auth/SignUpCompletePage.style.tsx
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+
+export const SignUpCompletePageWrapper = styled.section`
+    width: min(100%, 440px);
+    margin: 0px auto;
+`;
+
+export const SignUpCompletePageHeader = styled.p`
+    margin: 50px 0px;
+
+    text-align: center;
+`;
+
+export const SignUpCompleteContainer = styled.div`
+    text-align: center;
+    width: 100%;
+`;
+
+export const SignUpCompleteTextItem = styled.div`
+    justify-content: center;
+    width: 100%;
+    margin: 44px auto;
+`;
+
+export const SignUpCompleteItem = styled.div`
+    justify-content: center;
+    width: 100%;
+    margin: 12px auto;
+`;
--- src/pages/auth/SignUpCompletePage.tsx
+++ src/pages/auth/SignUpCompletePage.tsx
@@ -1,3 +1,59 @@
+import { Button } from "@/common/form/Button";
+import { Text } from "@/common/typography/Text";
+import { Title } from "@/common/typography/Title";
+
+import {
+    SignUpCompleteContainer,
+    SignUpCompleteItem,
+    SignUpCompletePageHeader,
+    SignUpCompletePageWrapper,
+    SignUpCompleteTextItem,
+} from "./SignUpCompletePage.style";
+
 export default function SignUpCompletePage() {
-    return <></>;
+    return (
+        <SignUpCompletePageWrapper>
+            <SignUpCompletePageHeader>
+                <Title>가입완료</Title>
+            </SignUpCompletePageHeader>
+            <SignUpCompleteContainer>
+                {/* <SignUpCompleteItem>
+                    <img src="src\assets\auth\congrats2.png" width="312px" height="230px" />
+                </SignUpCompleteItem> */}
+                <SignUpCompleteTextItem>
+                    <Text size="lx" color="point" weight="bold">
+                        환영합니다.
+                    </Text>
+                    <br />
+                    <Text size="lx" color="primary" weight="bold">
+                        가입이 완료되었어요.
+                    </Text>
+                </SignUpCompleteTextItem>
+                <SignUpCompleteItem>
+                    <Button
+                        variant="secondary"
+                        width="440px"
+                        height="54px"
+                        onClick={() => {
+                            //dispatch(pageActions.nextPage());
+                        }}
+                    >
+                        로그인하기
+                    </Button>
+                </SignUpCompleteItem>
+                <SignUpCompleteItem>
+                    <Button
+                        variant="primary"
+                        width="440px"
+                        height="54px"
+                        onClick={() => {
+                            //dispatch(pageActions.nextPage());
+                        }}
+                    >
+                        내 정보 등록하러 가기
+                    </Button>
+                </SignUpCompleteItem>
+            </SignUpCompleteContainer>
+        </SignUpCompletePageWrapper>
+    );
 }
