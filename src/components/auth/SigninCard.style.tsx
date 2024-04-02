2618bb90eccc5941e4834e2d119f9bebeaed5a73
chae-won-shin <scwcart@naver.com>
2024-04-03 01:49:05 +0900

    GETP-119 feature: 로그인 페이지 구현

---
+++ src/components/auth/SigninCard.style.tsx
@@ -0,0 +1,83 @@
+import { CheckBoxContainer } from "@/pages/auth/SignUpInputPage.style";
+
+import { place_center } from "@/styles/utils";
+
+import styled from "@emotion/styled";
+
+export interface ISeek {
+    variant: "seek" | "signup";
+}
+
+export const SigninCardBody = styled.div`
+    ${place_center}
+    display: flex;
+    align-items: center;
+
+    border-radius: 12px;
+    border: none;
+    box-shadow: 10px 20px 40px 0px rgba(0, 0, 0, 0.15);
+`;
+
+export const SigninCardWrapper = styled.div`
+    width: 430px;
+    height: 830px;
+    z-index: 10000;
+
+    border-radius: 12px;
+    border: none;
+    box-shadow: 10px 20px 40px 0px rgba(0, 0, 0, 0.15);
+    margin-left: -80px;
+    margin-top: -40px;
+
+    padding: 45px;
+
+    background-color: #fff;
+`;
+
+export const SigninHeader = styled.p`
+    margin: 60px 0px;
+`;
+
+export const SigninContainer = styled.div`
+    width: 100%;
+`;
+
+export const SigninItem = styled.div`
+    width: 100%;
+    margin: 30px 0px;
+`;
+
+export const SeekBtn = styled.button<ISeek>`
+    width: 100px;
+    height: 25px;
+
+    font-weight: bold;
+    flex-shrink: 0;
+
+    background-color: transparent;
+    color: ${(props) => {
+        switch (props.variant) {
+            case "seek":
+                return "#676f7c";
+            case "signup":
+                return "#476FF1";
+        }
+    }};
+`;
+
+export const GotoSignup = styled.div`
+    ${place_center}
+
+    width: 100%;
+    height: 54px;
+
+    margin: 220px 0px;
+
+    border-radius: 12px;
+
+    background-color: #f8f6f8;
+
+    color: #000;
+
+    border: none;
+`;
--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,3 +1,9 @@
+import { SigninCard } from "@/components/auth/SigninCard";
+
 export default function SignInPage() {
-    return <main>로그인 페이지</main>;
+    return (
+        <>
+            <SigninCard />
+        </>
+    );
 }
