2def1c97af94b0c84fba2d51f8fcbc0865a3ab36
toothlessdev <toothless042@gmail.com>
2024-04-08 02:58:49 +0900

    GETP-119 feature: NavBar 컴포넌트 구현

--- src/common/layout/MainLayout.tsx
+++ src/common/layout/MainLayout.tsx
@@ -7,12 +7,13 @@ import styled from "@emotion/styled";
 export const Main = styled.main`
     width: min(100%, 1440px);
     margin: 0px auto;
+    padding-top: 80px;
 `;
 
 export const MainLayout = () => {
     return (
         <>
-            <Header height="80px" padding="2rem 4rem" gap="25rem" containerGap="1.25rem" />
+            <Header />
             <Main>
                 <Outlet />
             </Main>
