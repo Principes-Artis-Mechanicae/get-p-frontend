7080e9a9a338c4ca52011ac0ad3bc2433422704c
toothlessdev <toothless042@gmail.com>
2024-04-08 10:16:40 +0900

    GETP-119 refactor: Footer 반응형 컴포넌트 리팩토링

---
+++ src/common/layout/Footer.style.tsx
@@ -0,0 +1,57 @@
+import { mobile, tablet } from "@/styles/breakpoint";
+
+import styled from "@emotion/styled";
+
+export const FooterWrapper = styled.footer`
+    width: 100%;
+    background-color: #f9fafa;
+`;
+
+export const FooterContainer = styled.div`
+    display: flex;
+    gap: 10px;
+    width: min(100%, 1400px);
+
+    margin: 0px auto;
+    padding: 20px;
+    padding-bottom: 50px;
+
+    ${tablet} {
+        flex-direction: column;
+    }
+    ${mobile} {
+        flex-direction: column;
+    }
+`;
+
+export const FooterIcon = styled.img`
+    display: block;
+    width: 100px;
+    height: 100%;
+
+    margin: 10px;
+
+    ${tablet} {
+        margin: 10px 0px;
+    }
+    ${mobile} {
+        margin: 10px 0px;
+    }
+`;
+
+export const FooterContainerHead = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+`;
+
+export const FooterContainerBody = styled.div`
+    display: flex;
+    gap: 20px;
+`;
+
+export const FooterItems = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+`;
--- src/common/layout/Footer.tsx
+++ src/common/layout/Footer.tsx
@@ -1,82 +1,86 @@
 import { Text } from "../typography/Text";
-import logo from "/logo.svg";
-import styled from "@emotion/styled";
+import {
+    FooterContainerHead,
+    FooterContainerBody,
+    FooterWrapper,
+    FooterItems,
+    FooterIcon,
+    FooterContainer,
+} from "./Footer.style";
 
-export const FooterWrapper = styled.footer`
-    width: 100%;
-    height: 12rem;
-    padding: 3rem 12rem;
-    gap: 7rem;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-export const FooterContainer = styled.div`
-    display: inline-flex;
-    align-items: flex-start;
-    word-break: keep-all;
-    gap: 1rem;
-`;
-
-export const FooterItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    flex-shrink: 0;
-    user-select: none;
-    gap: 0.4rem;
-`;
-
-export const Footer: React.FC = () => {
+export const Footer = () => {
     return (
-        <>
-            <FooterWrapper>
-                <img src={logo} alt="logo" />
-                <FooterItem>
-                    <Text size="s" color="secondary" weight="normal">
-                        상호명(주)겟피 대표이사:KIM BLABLA 개인정보책임관리자:김태우 주소:서울특별시 강남구 테헤란로
-                        415, L7 강남타워 5층
-                    </Text>
-                    <Text size="s" color="secondary" weight="normal">
-                        사업자등록번호:000-00-00000 통신판매업신고증:제 0000-서울강남-00000 호 직업정보제공사업
-                        신고번호:대구청 제 0000--00호
-                    </Text>
-                    <Text size="s" color="placeholder" weight="normal">
-                        Copyright ©Getp Inc. All Rights Reserved.
-                    </Text>
-                </FooterItem>
-                <FooterContainer>
-                    <Text size="s" color="placeholder" weight="normal">
-                        상담시간
-                    </Text>
-                    <FooterItem>
-                        <Text size="s" color="secondary" weight="normal">
-                            평일 09:00~18:00
+        <FooterWrapper>
+            <FooterContainer>
+                <FooterIcon src="/logo.svg" />
+
+                <FooterContainerHead>
+                    <p>
+                        <Text size="s" color="#676F7C">
+                            상호명:(주)겟피 · 대표이사:KIM BLABLA · 개인정보책임관리자:김태우 · 주소:서울특별시 강남구
+                            테헤란로 415, L7 강남타워 5층
                         </Text>
-                        <Text size="s" color="secondary" weight="normal">
-                            점심 12:00~13:00
+                    </p>
+                    <p>
+                        <Text size="s" color="#676F7C">
+                            사업자등록번호:000-00-00000 · 통신판매업신고증:제 0000-서울강남-00000 호 · 직업정보제공사업
+                            신고번호:대구청 제 0000-00호
                         </Text>
-                        <Text size="s" color="secondary" weight="normal">
-                            (주말, 공휴일은 제외)
+                    </p>
+                    <p>
+                        <Text size="s" color="#C4C7CC">
+                            Copyright ©Getp Inc. All Rights Reserved.
                         </Text>
-                    </FooterItem>
-                </FooterContainer>
-                <FooterContainer>
-                    <Text size="s" color="placeholder" weight="normal">
-                        Contact
-                    </Text>
-                    <FooterItem>
-                        <Text size="s" color="secondary" weight="normal">
-                            1599-0000
+                    </p>
+                </FooterContainerHead>
+
+                <FooterContainerBody>
+                    <p>
+                        <Text size="s" weight="bold" color="#676F7C">
+                            상담시간
                         </Text>
-                        <Text size="s" color="secondary" weight="normal">
-                            support@getp.com
+                    </p>
+
+                    <FooterItems>
+                        <p>
+                            <Text size="s" color="#676F7C">
+                                평일 09:00~18:00
+                            </Text>
+                        </p>
+                        <p>
+                            <Text size="s" color="#676F7C">
+                                점심 12:00~13:30
+                            </Text>
+                        </p>
+                        <p>
+                            <Text size="s" color="#676F7C">
+                                (주말 공휴일은 제외)
+                            </Text>
+                        </p>
+                    </FooterItems>
+                </FooterContainerBody>
+
+                <FooterContainerBody>
+                    <p>
+                        <Text size="s" weight="bold" color="#676F7C">
+                            Contact
                         </Text>
-                    </FooterItem>
-                </FooterContainer>
-            </FooterWrapper>
-        </>
+                    </p>
+
+                    <FooterItems>
+                        <p>
+                            <Text size="s" color="#676F7C">
+                                1599-0000
+                            </Text>
+                        </p>
+                        <p>
+                            <Text size="s" color="#676F7C">
+                                support@getp.com
+                            </Text>
+                        </p>
+                    </FooterItems>
+                </FooterContainerBody>
+            </FooterContainer>
+        </FooterWrapper>
     );
 };
