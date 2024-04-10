eda035e841f1a3073c0ac77264ff4a0c92fc7881
toothlessdev <toothless042@gmail.com>
2024-04-10 20:57:30 +0900

    GETP-122 chore: 사용하지 않는 Import 제거

--- src/components/auth/SelectTypeSection.tsx
+++ src/components/auth/SelectTypeSection.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 import { motion } from "framer-motion";
 
