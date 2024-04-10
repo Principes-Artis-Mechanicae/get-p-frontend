a0d4d9b001dc2e89733a4f2a9ae2285d5c222dcf
toothlessdev <toothless042@gmail.com>
2024-04-10 21:00:41 +0900

    GETP-122 style: Z-Index 상수로 분리

--- src/styles/zindex.ts
+++ src/styles/zindex.ts
@@ -1,3 +1,5 @@
-export const Z_NAVBAR = 10001;
-export const Z_NAV_ASIDE_BACKDROP = 9998;
-export const Z_NAV_ASIDE_WRAPPER = 9999;
+export const Z_TOAST = 9999; // React-Toastify ToastContainer Z_INDEX
+
+export const Z_NAVBAR = 9998;
+export const Z_NAV_ASIDE_WRAPPER = 9997;
+export const Z_NAV_ASIDE_BACKDROP = 9996;
