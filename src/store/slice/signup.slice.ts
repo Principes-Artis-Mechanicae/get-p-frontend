b3829a473cec044dd99eff391e92b1b0b8e9a2fe
toothlessdev <toothless042@gmail.com>
2024-04-27 13:14:07 +0900

    GETP-123 feature: signUpThunkAction 구현

--- src/store/slice/signup.slice.ts
+++ src/store/slice/signup.slice.ts
@@ -1,4 +1,7 @@
-import { RootDispatch } from "../store";
+import { authService } from "@/services/auth/auth.service";
+import { MemberType } from "@/services/auth/auth.types";
+
+import { RootDispatch, store } from "../store";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export interface ISignUpState {
@@ -6,6 +9,7 @@ export interface ISignUpState {
     signUpMemberType: string | null;
     signUpEmailValue: string | null;
     signUpPasswordValue: string | null;
+    signUpVerificationCode: string | null;
     signUpAgreementValue: boolean;
 }
 
@@ -14,10 +18,11 @@ const initialState: ISignUpState = {
     signUpMemberType: null,
     signUpEmailValue: null,
     signUpPasswordValue: null,
+    signUpVerificationCode: null,
     signUpAgreementValue: false,
 };
 
-export const signUpSlice = createSlice({
+const signUpSlice = createSlice({
     name: "getp/signup",
     initialState,
 
@@ -27,6 +32,7 @@ export const signUpSlice = createSlice({
             state.signUpEmailValue = null;
             state.signUpPasswordValue = null;
             state.signUpAgreementValue = false;
+            state.signUpVerificationCode = null;
         },
 
         initializeSection: (state) => {
@@ -51,14 +57,39 @@ export const signUpSlice = createSlice({
         setSignUpAgreementValue: (state, action: PayloadAction<boolean>) => {
             state.signUpAgreementValue = action.payload;
         },
+        setVerificationCode: (state, action: PayloadAction<string>) => {
+            state.signUpVerificationCode = action.payload;
+        },
     },
 });
 
 export const signUpAction = signUpSlice.actions;
+export const signUpReducer = signUpSlice.reducer;
 
 export const signUpThunkAction = () => {
-    return (dispatch: RootDispatch) => {
+    return async (dispatch: RootDispatch, getState: typeof store.getState) => {
         // 회원가입 POST 요청
+
+        const {
+            signUpMemberType,
+            signUpEmailValue,
+            signUpPasswordValue,
+            signUpAgreementValue,
+            signUpVerificationCode,
+        } = getState().signUp;
+
+        await authService.signUp({
+            memberType: signUpMemberType as MemberType,
+            email: signUpEmailValue as string,
+            password: signUpPasswordValue as string,
+            verificationCode: signUpVerificationCode as string,
+            serviceTerms: [
+                {
+                    tag: "GET-P 서비스 약관",
+                    agreed: signUpAgreementValue,
+                },
+            ],
+        });
 
         dispatch(signUpAction.nextSignUpSection());
         dispatch(signUpAction.initalizeState());
