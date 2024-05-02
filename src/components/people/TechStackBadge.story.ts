b7efb92a70f7ef1d42fd21f8c6e1cce8db94fdff
toothlessdev <toothless042@gmail.com>
2024-05-03 02:18:21 +0900

    GETP-124 feature: TechStackBadge 컴포넌트 구현

---
+++ src/components/people/TechStackBadge.story.ts
@@ -0,0 +1,16 @@
+import { TechStackBadge } from "./TechStackBadge";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+    title: "People/TechStackBadge",
+    component: TechStackBadge,
+} satisfies Meta<typeof TechStackBadge>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        text: "JavaScript",
+    },
+};
---
+++ src/components/people/TechStackBadge.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useRef } from "react";
+
+import { Paragraph } from "@/common/typography/Paragraph";
+
+import closeIcon from "@/assets/people/close.svg";
+
+import { TechStackBadgeCloseIcon, TechStackBadgeWrapper } from "./TechStackBadge.style";
+import { useTechStack } from "@/contexts/TechStackContext";
+
+export interface ITechStackBadge {
+    text: string;
+}
+
+export const TechStackBadge: React.FC<ITechStackBadge> = ({ text }) => {
+    const { dispatch } = useTechStack();
+
+    const handleBadgeClick = useCallback(() => {
+        dispatch({
+            type: "REMOVE_TECH_STACK",
+            payload: {
+                value: text,
+            },
+        });
+    }, [dispatch, text]);
+
+    return (
+        <TechStackBadgeWrapper>
+            <Paragraph size="m" weight="bold">
+                {text}
+            </Paragraph>
+            <TechStackBadgeCloseIcon src={closeIcon} onClick={handleBadgeClick} />
+        </TechStackBadgeWrapper>
+    );
+};
