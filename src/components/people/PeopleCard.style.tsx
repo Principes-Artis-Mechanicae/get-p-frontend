13522f48ca2dcad71ae3ce380d0a0a1e8fbcbdbc
zladb <zla_db@naver.com>
2024-05-05 22:41:07 +0900

    GETP-127 feat: PeopleCard 컴포넌트 구현

---
+++ src/components/people/PeopleCard.style.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+
+export interface IPeopleCardWrapper {
+    width: string;
+    height: string;
+}
+
+export interface IPeopleCardImg {
+    src: string;
+}
+
+export const PeopleCardWrapper = styled.div<IPeopleCardWrapper>`
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
+
+    padding: 24px;
+
+    border-radius: 12px;
+    border: 1px solid #ebedef;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 15px;
+`;
+
+export const PeopleCardImg = styled.img<IPeopleCardImg>`
+    src: ${(props) => props.src};
+    height: 100%;
+`;
+
+export const PeopleCardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    gap: 8px;
+    text-align: left;
+    width: 100%;
+    height: 100%;
+`;
+
+export const PeopleCardHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+    width: 100%;
+`;
+
+export const PeopleCardComment = styled.div`
+    width: 370px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`;
