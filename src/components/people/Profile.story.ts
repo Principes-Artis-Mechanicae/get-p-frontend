8e3a31ebcfef10f279e8e8dde3cd5187c9300971
chominju <cmj021002@knu.ac.kr>
2024-05-06 17:03:58 +0900

    GETP-128 feature: Profile 컴포넌트 구현

---
+++ src/components/people/Profile.story.ts
@@ -0,0 +1,21 @@
+import { Profile } from "./Profile";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+    title: "People/Profile",
+    component: Profile,
+} satisfies Meta<typeof Profile>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        width: "268x",
+        height: "283px",
+        nickname: "People01",
+        likeCount: 14,
+        completeProjectsCount: 2,
+    },
+};
