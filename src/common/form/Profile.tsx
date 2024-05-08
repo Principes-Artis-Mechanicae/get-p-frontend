ccb6cf0022eb7acf4310e84ed70a5345ab258861
chae-won-shin <scwcart@naver.com>
2024-05-08 16:25:51 +0900

    Feat: 피플 프로필 사진 등록 기능 구현

--- src/common/form/Profile.tsx
+++ src/common/form/Profile.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, ChangeEvent } from "react";
 
 import { place_center } from "@/styles/utils";
 
@@ -21,7 +21,7 @@ const ProfileImage = styled.img`
     border-radius: 50%;
 `;
 
-const EditBtn = styled.button`
+const EditBtn = styled.label`
     ${place_center}
     position: absolute;
     bottom: 0;
@@ -35,20 +35,43 @@ const EditBtn = styled.button`
     border: none;
     border-radius: 50%;
     box-shadow: 2px 3px 9px rgba(0, 0, 0, 0.15);
+
+    cursor: pointer;
 `;
 
 const PencilIcon = styled.img`
     color: #8c929c;
 `;
 
+const InputFile = styled.input`
+    display: none;
+`;
+
 export const Profile = () => {
+    const [profileImage, setProfileImage] = useState(defaultImg);
+    const inputFileRef = useRef<HTMLInputElement>(null);
+
+    const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                const result = reader.result as string;
+                setProfileImage(result);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    };
     return (
         <>
             <ProfileContainer>
-                <ProfileImage src={defaultImg} alt="profile" />
-                <EditBtn>
+                <ProfileImage src={profileImage} alt="profile" />
+                <EditBtn htmlFor="upload-input">
                     <PencilIcon src={editIcon} alt="edit" />
                 </EditBtn>
+                <InputFile type="file" id="upload-input" ref={inputFileRef} onChange={handleImageUpload} />
             </ProfileContainer>
         </>
     );
