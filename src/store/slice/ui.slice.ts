15aaf34c81fc26a6f5b46d6351befdbc28ea7dad
toothlessdev <toothless042@gmail.com>
2024-05-11 02:46:34 +0900

    GETP-135 feat: NavDropDown 전역상태 설정

--- src/store/slice/ui.slice.ts
+++ src/store/slice/ui.slice.ts
@@ -1,11 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface IUserInterfaceState {
+    isProfileDropDownOpened: boolean;
     isNavAsideOpened: boolean;
     isModalOpened: boolean;
 }
 
 const initialState: IUserInterfaceState = {
+    isProfileDropDownOpened: false,
     isNavAsideOpened: false,
     isModalOpened: false,
 };
@@ -16,6 +18,13 @@ const uiSlice = createSlice({
     initialState,
 
     reducers: {
+        toggleNavDropDown: (state) => {
+            if (state.isProfileDropDownOpened) state.isProfileDropDownOpened = false;
+            else state.isProfileDropDownOpened = true;
+        },
+        closeNavDropDown: (state) => {
+            state.isProfileDropDownOpened = false;
+        },
         toggleNavAside: (state) => {
             if (state.isNavAsideOpened) state.isNavAsideOpened = false;
             else state.isNavAsideOpened = true;
