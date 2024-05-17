bbae3f0b0317024fd278e5aee2c8229a8f22c534
toothlessdev <toothless042@gmail.com>
2024-05-17 15:41:17 +0900

    GETP-136 feat: HomePage Layout 분리

--- src/common/layout/MainLayout.tsx
+++ src/common/layout/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
-import { Bounce, ToastContainer } from "react-toastify";
 
+import { Toast } from "../feedback/Toast";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
 import styled from "@emotion/styled";
 
 export const Main = styled.main`
     width: min(100%, 1440px);
+    min-height: 100vh;
+
     margin: 0px auto;
     padding: 20px;
     padding-top: 80px;
@@ -15,19 +17,7 @@ export const Main = styled.main`
 export const MainLayout = () => {
     return (
         <>
-            <ToastContainer
-                position="top-center"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-                transition={Bounce}
-            />
+            <Toast />
 
             <Header />
             <Main>
