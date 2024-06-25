60e16fff5cf404154785049d144b2a19967f12fd
toothlessdev <toothless042@gmail.com>
2024-06-25 22:13:09 +0900

    feat: Carousel ScrollBar 구현 (스타일 수정필요)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import "swiper/css";
+import "swiper/css/scrollbar";
 import { register } from "swiper/element/bundle";
 
 register();
