c22e51d954ec83205199c5e1e4944e8e0b595c4f
toothlessdev <toothless042@gmail.com>
2024-07-09 05:36:18 +0900

    chore: @tanstack/react-query 설치 및 queryClient 설정

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,11 @@ import PeopleInfoRegisterPage from "@/pages/people/PeopleInfoRegisterPage";
 import PeopleListPage from "@/pages/people/PeopleListPage";
 import PeopleProfileEditPage from "@/pages/people/PeopleProfileEditPage";
 
+import { queryClient } from "@/config/query";
+
 import "./globals.css";
 import { persistor, store } from "@/store/store";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { PersistGate } from "redux-persist/lib/integration/react";
 
 export default function App() {
@@ -26,27 +29,29 @@ export default function App() {
         <>
             <BrowserRouter>
                 <Provider store={store}>
-                    <PersistGate loading={null} persistor={persistor}>
-                        <AnimatePresence>
-                            <Routes>
-                                <Route index element={<HomePage />} />
+                    <QueryClientProvider client={queryClient}>
+                        <PersistGate loading={null} persistor={persistor}>
+                            <AnimatePresence>
+                                <Routes>
+                                    <Route index element={<HomePage />} />
 
-                                <Route path="/" element={<MainLayout />}>
-                                    <Route path="auth/signin" element={<SignInPage />}></Route>
-                                    <Route path="auth/signup" element={<SignUpPage />}></Route>
-                                    <Route path="auth/findpw" element={<FindPasswordPage />}></Route>
+                                    <Route path="/" element={<MainLayout />}>
+                                        <Route path="auth/signin" element={<SignInPage />}></Route>
+                                        <Route path="auth/signup" element={<SignUpPage />}></Route>
+                                        <Route path="auth/findpw" element={<FindPasswordPage />}></Route>
 
-                                    <Route path="people" element={<PeopleListPage />} />
-                                    <Route path="people/:id" element={<PeopleDetailPage />}></Route>
-                                    <Route path="people/register" element={<PeopleInfoRegisterPage />}></Route>
-                                    <Route path="people/edit" element={<PeopleProfileEditPage />} />
+                                        <Route path="people" element={<PeopleListPage />} />
+                                        <Route path="people/:id" element={<PeopleDetailPage />}></Route>
+                                        <Route path="people/register" element={<PeopleInfoRegisterPage />}></Route>
+                                        <Route path="people/edit" element={<PeopleProfileEditPage />} />
 
-                                    <Route path="client/register" element={<RegisterClientPage />} />
-                                    <Route path="client/edit" element={<EditClientPage />} />
-                                </Route>
-                            </Routes>
-                        </AnimatePresence>
-                    </PersistGate>
+                                        <Route path="client/register" element={<RegisterClientPage />} />
+                                        <Route path="client/edit" element={<EditClientPage />} />
+                                    </Route>
+                                </Routes>
+                            </AnimatePresence>
+                        </PersistGate>
+                    </QueryClientProvider>
                 </Provider>
             </BrowserRouter>
         </>
