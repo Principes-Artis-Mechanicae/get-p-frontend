e805de6dfa4c5c731481f2438a0a536b28066032
toothlessdev <toothless042@gmail.com>
2024-07-15 03:18:44 +0900

    refactor: queryClient staleTime, gcTime 설정

--- src/config/query.ts
+++ src/config/query.ts
@@ -3,7 +3,9 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
+            // 1분 뒤 STALE 상태로 변경
             staleTime: 60 * 1000,
+            // 캐시 데이터 5분 뒤 삭제
             gcTime: 60 * 5 * 1000,
         },
     },
