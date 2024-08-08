84b14ee23b0d1f2c6965292c480fd0f639a2b73c
toothlessdev <toothless042@gmail.com>
2024-08-09 01:41:16 +0900

    refactor/GETP-178: 피플 목록조회페이지 페이지네이션 적용, API 연동 및 리팩토링

--- src/pages/people/PeopleListPage.tsx
+++ src/pages/people/PeopleListPage.tsx
@@ -1,74 +1,41 @@
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-
-import { IPagination, Pagination } from "@/common/navigation/Pagination";
+import Pagination from "@/common/navigation/Pagination";
 
 import { PeopleCard } from "@/components/people/PeopleCard";
 import { PeopleSearch } from "@/components/people/PeopleSearch";
 
-import { peopleService } from "@/services/people/service";
-import { PeopleData } from "@/services/people/types";
+import { usePeopleList } from "@/services/people/usePeopleList";
 
 import { PeopleListContainer, PeopleListWrapper } from "./PeopleListPage.style";
 
 export default function PeopleListPage() {
-    const [people, setPeople] = useState<PeopleData[] | null>(null);
+    const { isPending, data } = usePeopleList();
 
-    const [searchParams] = useSearchParams();
-    const [pageInfo, setPageInfo] = useState<IPagination>({
-        totalItems: 10,
-        itemCountPerPage: 10,
-        pageCount: 10,
-        currentPage: parseInt(searchParams.get("page") ?? "1"),
-        basePath: "/people",
-    });
-
-    // searchParams가 바뀔 때 마다 새로운 페이지로 api 호출함.
-    useEffect(() => {
-        const currentPage = parseInt(searchParams.get("page") ?? "1");
-        const itemCountPerPage = parseInt(searchParams.get("size") ?? "10");
-
-        peopleService.readPeople(currentPage, itemCountPerPage).then((value) => {
-            setPeople(value.content);
-            setPageInfo((prevPageInfo) => ({
-                ...prevPageInfo,
-                totalItems: value.pageInfo.totalElements,
-                itemCountPerPage: value.pageInfo.size,
-                pageCount: 10,
-                currentPage: value.pageInfo.number,
-                basePath: "/people",
-            }));
-        });
-    }, [searchParams]);
+    if (isPending) return <>loading...</>;
 
     return (
         <PeopleListWrapper>
             <PeopleSearch width="100%" height="auto" />
             <PeopleListContainer>
-                {people && (
+                {data && (
                     <>
-                        {people.map((person) => (
+                        {data.content.map((people) => (
                             <PeopleCard
+                                key={people.peopleId}
                                 width="auto"
                                 height="auto"
-                                profileImageUri={person.profileImageUri}
-                                nickname={person.nickname}
-                                introduction={person.profile.introduction}
-                                activityArea={person.profile.activityArea}
-                                hashtags={person.profile.hashtags}
-                                completeProjectsCount={person.completedProjectsCount}
+                                profileImageUri={people.profileImageUri}
+                                nickname={people.nickname}
+                                introduction={people.profile.introduction}
+                                activityArea={people.profile.activityArea}
+                                hashtags={people.profile.hashtags}
+                                completeProjectsCount={people.completedProjectsCount}
                             />
                         ))}
                     </>
                 )}
             </PeopleListContainer>
-            <Pagination
-                totalItems={pageInfo.totalItems}
-                itemCountPerPage={pageInfo.itemCountPerPage}
-                pageCount={pageInfo.pageCount}
-                currentPage={pageInfo.currentPage && pageInfo.currentPage > 0 ? pageInfo.currentPage : 1}
-                basePath="/people"
-            />
+
+            <Pagination totalPages={data?.pageInfo.totalPages as number} pageGroupSize={5} />
         </PeopleListWrapper>
     );
 }
