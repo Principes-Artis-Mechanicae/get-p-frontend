acaa15bb20f227128710e84bff9a58b497b6584d
toothlessdev <toothless042@gmail.com>
2024-08-09 19:40:23 +0900

    refactor/GETP-179: 피플정보 등록시 people 도메인 쿼리 stale 상태로 변경하도록 수정

--- src/services/people/keys.ts
+++ src/services/people/keys.ts
@@ -1,4 +1,5 @@
 export const PEOPLE_QUERY_KEYS = {
+    PEOPLE: () => ["people"],
     READ_PEOPLE: (page?: number, size?: number, sort?: string) => ["people", { page, size, sort }],
     READ_PEOPLE_BY_ID: (id: number) => ["people", { id }],
 };
