1e967155bac02cef7a136fb18c85586d58b1a08c
toothlessdev <toothless042@gmail.com>
2024-08-09 17:43:33 +0900

    chore/GETP-179: MSW Mocking 디렉토리 src 내부로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ async function enableMocking(enable: boolean) {
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const { worker }: { worker: { start: () => Promise<void> } } = await import("../mocks/browser");
+    const { worker }: { worker: { start: () => Promise<void> } } = await import("./mocks/browser.js");
     return worker.start();
 }
 
