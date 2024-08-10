722d1da29a68a653c854c2c1b86052e24eeabc5d
toothlessdev <toothless042@gmail.com>
2024-08-11 01:58:32 +0900

    refactor/GETP-165: 피플 상세정보 Mocking

--- src/mocks/people/handlers.ts
+++ src/mocks/people/handlers.ts
@@ -1,10 +1,10 @@
 import { HttpResponse, http } from "msw";
 
-import { peopleData } from "./data";
+import { peopleData, peopleDetailedData } from "./data";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "https://api.princip.es/get-p/v2";
 
-export const readPeople = http.get("https://api.princip.es/get-p/v2/people", ({ request }) => {
+export const readPeople = http.get(API_BASE_URL + "/people", ({ request }) => {
     const url = new URL(request.url);
 
     const pageParam = url.searchParams.get("page");
@@ -51,10 +51,7 @@ export const readPeople = http.get("https://api.princip.es/get-p/v2/people", ({ 
 export const readPeopleById = http.get(API_BASE_URL + `/people/:id`, async ({ params }) => {
     const { id } = params;
 
-    return HttpResponse.json({
-        status: 200,
-        data: peopleData[+id],
-    });
+    return HttpResponse.json(peopleDetailedData[+id - 1]);
 });
 
 export const registerPeopleInfo = http.post(API_BASE_URL + "people/me", async () => {
