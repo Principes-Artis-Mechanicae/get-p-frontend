8428ec27cff7aa82e8e5579d4c333be031b327ad
Yujin <zla_db@naver.com>
2024-08-16 13:13:34 +0900

    feat: project mock data와handler 구현

---
+++ src/mocks/project/data.ts
@@ -0,0 +1,44 @@
+export const projectData = [
+    {
+        projectId: 1,
+        title: "프로젝트 제목",
+        payment: 1000000,
+        applicantsCount: 5,
+        estimatedDays: 10,
+        applicationDuration: {
+            startDate: "2024-07-01",
+            endDate: "2024-07-07",
+        },
+        hashtags: ["#해시태그1", "#해시태그2"],
+        description: "프로젝트 설명",
+        status: "APPLYING",
+    },
+    {
+        projectId: 1,
+        title: "프로젝트 제목",
+        payment: 1000000,
+        applicantsCount: 5,
+        estimatedDays: 10,
+        applicationDuration: {
+            startDate: "2024-07-01",
+            endDate: "2024-07-07",
+        },
+        hashtags: ["#해시태그1", "#해시태그2"],
+        description: "프로젝트 설명",
+        status: "APPLYING",
+    },
+    {
+        projectId: 1,
+        title: "프로젝트 제목",
+        payment: 1000000,
+        applicantsCount: 5,
+        estimatedDays: 10,
+        applicationDuration: {
+            startDate: "2024-07-01",
+            endDate: "2024-07-07",
+        },
+        hashtags: ["#해시태그1", "#해시태그2"],
+        description: "프로젝트 설명",
+        status: "APPLYING",
+    },
+];
