199e905553b7d8bdb0faf2f9f0067844efdba86b
toothlessdev <toothless042@gmail.com>
2024-09-01 02:29:00 +0900

    feature/GETP-207: ProjectTag 컴포넌트 구현

---
+++ src/components/project/ProjectTag/ProjectTag.style.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const ProjectTagContainer = styled.div`
+    width: 100%;
+    margin: 5px 0px;
+`;
+
+export const ProjectTag = styled.span`
+    display: inline-flex;
+
+    margin: 5px;
+    padding: 10px;
+
+    border-radius: 10px;
+
+    font-weight: bold;
+    color: #476ff1;
+    background-color: #ecf0fe;
+`;
