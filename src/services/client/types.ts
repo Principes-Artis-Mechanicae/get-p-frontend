341fc67a29a15b41f65e699b54cec9cdcca6f53a
toothlessdev <toothless042@gmail.com>
2024-09-01 04:37:00 +0900

    feature/GETP-215: RegisterClient API 타입 추가

---
+++ src/services/client/types.ts
@@ -0,0 +1,21 @@
+import { BaseResponse } from "../types";
+
+export type RegisterClientRequestBody = {
+    nickname: string;
+    email: string;
+    phoneNumber: string;
+    address: {
+        zipcode: string;
+        street: string;
+        detail: string;
+    };
+    bankAccount: {
+        bank: string;
+        accountNumber: string;
+        accountHolder: string;
+    };
+};
+
+export type RegisterClientResponseBody = BaseResponse<{
+    clientId: number;
+}>;
