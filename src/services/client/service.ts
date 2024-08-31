4123c730de1fcb022f48e14397c9762b495ea608
toothlessdev <toothless042@gmail.com>
2024-09-01 04:12:33 +0900

    feature/GETP-215: 의뢰자 등록 및 수정 API 연동

---
+++ src/services/client/service.ts
@@ -0,0 +1,39 @@
+import { toast } from "react-toastify";
+
+import { api } from "@/config/axios";
+
+import { ExceptionHandler } from "@/utils/exception";
+
+import { RenderToastFromDerivedError } from "../exception";
+import { RegisterClientRequestBody, RegisterClientResponseBody } from "./types";
+
+export const clientService = {
+    registerClient: async (body: RegisterClientRequestBody) => {
+        const request = async () => {
+            const response = await api.post<RegisterClientResponseBody>("/client/me", body);
+
+            return new ExceptionHandler.Builder(response)
+                .addCase(500, "의뢰자 정보 등록중 오류가 발생하였습니다. 잠시 후 다시 시도해 주세요")
+                .activate();
+        };
+
+        return toast.promise(request, {
+            success: "의뢰자 정보 등록 완료!",
+            pending: "의뢰자 정보 등록 중입니다",
+            error: RenderToastFromDerivedError,
+        });
+    },
+    editClient: async (body: RegisterClientRequestBody) => {
+        const request = async () => {
+            const response = await api.put<RegisterClientResponseBody>("/client/me", body);
+            return new ExceptionHandler.Builder(response)
+                .addCase(500, "의뢰자 정보 등록중 오류가 발생하였습니다. 잠시 후 다시 시도해 주세요")
+                .activate();
+        };
+        return toast.promise(request, {
+            success: "의뢰자 정보 수정 완료!",
+            pending: "의뢰자 정보 수정 중입니다",
+            error: RenderToastFromDerivedError,
+        });
+    },
+};
---
+++ src/services/client/useEditClient.tsx
@@ -0,0 +1,35 @@
+import { useCallback, useRef } from "react";
+
+import { clientService } from "./service";
+import { useMutation } from "@tanstack/react-query";
+
+export const useEditClient = () => {
+    const nicknameRef = useRef<HTMLInputElement>(null);
+    const emailRef = useRef<HTMLInputElement>(null);
+    const phoneNumberRef = useRef<HTMLInputElement>(null);
+
+    const mutation = useMutation({
+        mutationFn: () =>
+            clientService.editClient({
+                nickname: nicknameRef.current?.value as string,
+                email: emailRef.current?.value as string,
+                phoneNumber: phoneNumberRef.current?.value as string,
+                address: {
+                    zipcode: "",
+                    street: "",
+                    detail: "",
+                },
+                bankAccount: {
+                    bank: "",
+                    accountNumber: "",
+                    accountHolder: "",
+                },
+            }),
+    });
+
+    const handleRegisterBtnClick = useCallback(() => {
+        mutation.mutate();
+    }, [mutation]);
+
+    return { nicknameRef, emailRef, phoneNumberRef, handleRegisterBtnClick };
+};
