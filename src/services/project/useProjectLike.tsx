baa8caa98112d416559a99046d6fcd8bc280dd53
toothlessdev <toothless042@gmail.com>
2024-09-02 23:08:50 +0900

    chore/GETP-205: MSW 디렉토리 변경 및 StarIcon 에셋 추가

--- src/services/project/useProjectLike.tsx
+++ src/services/project/useProjectLike.tsx
@@ -4,17 +4,17 @@ import { useParams } from "react-router-dom";
 import { projectService } from "./service";
 import { useMutation } from "@tanstack/react-query";
 
-export const useLikeClick = () => {
-    const [likeClick, setLikeClick] = useState<boolean>(false);
+export const useProjectLike = () => {
+    const [isActive, setIsActive] = useState<boolean>(false);
     const { id } = useParams();
 
     const mutation = useMutation({
         mutationFn: () =>
-            likeClick ? projectService.registerProjectLike(Number(id)) : projectService.deleteProjectLike(Number(id)),
+            isActive ? projectService.registerProjectLike(Number(id)) : projectService.deleteProjectLike(Number(id)),
     });
 
     const handleLikeClick = useCallback(() => {
-        setLikeClick((prev) => {
+        setIsActive((prev) => {
             const newLikeClick = !prev;
             mutation.mutate();
             return newLikeClick;
@@ -22,7 +22,7 @@ export const useLikeClick = () => {
     }, [mutation]);
 
     return {
-        likeClick,
+        isActive,
         handleLikeClick,
     };
 };
