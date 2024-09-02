15e8a539233d8ca5d9316913161ca8b817759245
toothlessdev <toothless042@gmail.com>
2024-09-02 23:07:04 +0900

    feature/GETP-205: AuthFilter 컴포넌트 구현

--- src/components/__common__/guard/AuthFilterGuard.tsx
+++ src/components/__common__/guard/AuthFilterGuard.tsx
@@ -4,13 +4,17 @@ import { MemberType } from "@/services/auth/types";
 
 import { RootState } from "@/store/store";
 
-export interface IAuthFilter {
-    role: MemberType | null;
-    children: React.ReactNode;
+export interface AuthFilterProps {
+    forClient: React.ReactNode;
+    forPeople: React.ReactNode;
+    forUnauthorized: React.ReactNode;
 }
 
-export const AuthFilterGuard: React.FC<IAuthFilter> = ({ role, children }) => {
+export const AuthFilter = ({ forClient, forPeople, forUnauthorized }: AuthFilterProps) => {
     const { memberType } = useSelector((state: RootState) => state.auth);
-    console.log(memberType);
-    if (memberType === role) return <>{children}</>;
+
+    if (memberType === MemberType.ROLE_CLIENT) return forClient;
+    else if (memberType === MemberType.ROLE_PEOPLE) return forPeople;
+    else if (memberType === null) return forUnauthorized;
+    else return null;
 };
