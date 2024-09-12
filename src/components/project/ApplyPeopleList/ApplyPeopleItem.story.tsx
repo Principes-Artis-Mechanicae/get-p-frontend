88b43256a0199faf395ad86171a9608475da6361
toothlessdev <toothless042@gmail.com>
2024-09-12 16:36:59 +0900

    GETP-226 chore: ApplyPeopleItem profileImage undefined 로 설정

--- src/components/project/ApplyPeopleList/ApplyPeopleItem.story.tsx
+++ src/components/project/ApplyPeopleList/ApplyPeopleItem.story.tsx
@@ -12,7 +12,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
     args: {
         id: 1,
-        profileImage: null,
         name: "방민혁",
         school: "한양대학교(서울)",
         education: "산업공학과 2학년",
