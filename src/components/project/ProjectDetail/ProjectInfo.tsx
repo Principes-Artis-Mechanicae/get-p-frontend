4d6685d49d0aaea4fa518050dba6f0ad0ab29e59
chominju <cmj021002@knu.ac.kr>
2024-10-06 19:41:40 +0900

    feature/GETP-297: 누락된 모집필드, 작업관련 변수 추가

--- src/components/project/ProjectDetail/ProjectInfo.tsx
+++ src/components/project/ProjectDetail/ProjectInfo.tsx
@@ -4,18 +4,35 @@ import {
     ProjectInfoWrapper,
     ProjectHeaderContainer,
     ProjectDueDate,
+    ProjectInfoContainer,
+    ProjectTextContainer,
     ProjectHashTagContainer,
     ProjectHashTag,
     Line,
 } from "./ProjectInfo.style";
 
 export interface ProjectInfoProps {
+    totalDays: number;
     remainedDays: number;
     title: string;
     hashtags: string[];
+    recruitmentCount: number;
+    applicantsCount: number;
+    applicationDuration: {
+        startDate: string;
+        endDate: string;
+    };
 }
 
-export const ProjectInfo = ({ remainedDays, title, hashtags }: ProjectInfoProps) => {
+export const ProjectInfo = ({
+    totalDays,
+    remainedDays,
+    title,
+    hashtags,
+    recruitmentCount,
+    applicantsCount,
+    applicationDuration,
+}: ProjectInfoProps) => {
     return (
         <ProjectInfoWrapper>
             <ProjectHeaderContainer>
@@ -24,10 +41,33 @@ export const ProjectInfo = ({ remainedDays, title, hashtags }: ProjectInfoProps)
                     오프라인 미팅
                 </Text>
             </ProjectHeaderContainer>
-            <Text size="22px" weight="bold">
-                {title}
-            </Text>
+
+            <ProjectInfoContainer>
+                <Text size="22px" weight="bold">
+                    {title}
+                </Text>
+            </ProjectInfoContainer>
+
+            <ProjectInfoContainer>
+                <Text size="m" color="secondary" weight="bold">
+                    {applicantsCount}명 지원 / {recruitmentCount}명 모집
+                </Text>
+            </ProjectInfoContainer>
+
+            <ProjectTextContainer>
+                <Text size="m" weight="bold">
+                    예상 작업 기간: {totalDays}일
+                </Text>
+                <Text size="m" weight="normal">
+                    프로젝트 모집 시작일: {applicationDuration.startDate}일
+                </Text>
+                <Text size="m" weight="normal">
+                    프로젝트 모집 마감일: {applicationDuration.endDate}일
+                </Text>
+            </ProjectTextContainer>
+
             <Line />
+
             <ProjectHashTagContainer>
                 {hashtags?.map((hashtag, index) => {
                     return <ProjectHashTag key={index}>{hashtag}</ProjectHashTag>;
