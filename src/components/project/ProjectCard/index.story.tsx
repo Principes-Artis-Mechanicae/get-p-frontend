dd64912848583114db4681d9b49dc5d38ad2ee8b
toothlessdev <toothless042@gmail.com>
2024-10-14 03:32:03 +0900

    feature: ProjectCardSkeleton 컴포넌트 구현

--- src/components/project/ProjectCard/index.story.tsx
+++ src/components/project/ProjectCard/index.story.tsx
@@ -1,4 +1,4 @@
-import { ProjectCard } from ".";
+import { ProjectCard, ProjectCardSkeleton } from ".";
 
 import type { Meta, StoryObj } from "@storybook/react";
 
@@ -111,4 +111,8 @@ export const CANCELLED: Story = {
             "React를 이용해 간단한 캘린더를 제작해주세요. 스토리 보드 및 기획은 이미 완성되어 있는 상태이며, 개발자의 시안 수령 및 구현만이 남아 있습니다. 자세한 api명세와 데이터베이스 관리에 대해서는 논의가 더 필요합니다. ",
         status: "CANCELLED",
     },
+};
+
+export const Skeleton = {
+    render: () => <ProjectCardSkeleton />,
 };
