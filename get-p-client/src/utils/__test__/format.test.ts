16ddc55d7238c931a6a5305da3a94cc9cb2ad844
toothlessdev <toothless042@gmail.com>
2024-10-17 17:09:44 +0900

    fix: 에러 반환 테스트 코드 수정

--- get-p-client/src/utils/__test__/format.test.ts
+++ get-p-client/src/utils/__test__/format.test.ts
@@ -11,13 +11,13 @@ describe("초 단위를 '분:초' 문자열로 변환합니다", () => {
 });
 
 describe("'년-월-일' 문자열을 [년, 월, 일] 숫자 배열로 반환합니다", () => {
-    // test("올바른 포맷의 경우 일치하는 Date 객체를 반환합니다", () => {
-    //     const str = "2000-04-02";
-    //     expect(formatDate(str)).toBe(new Date().setFullYear(2000, 4, 2));
-    // });
-
-    test("올바른 포맷이 아닐 경우 에러를 반환합니다", () => {
-        const str = "1-2";
-        expect(formatDate(str)).toThrow("date 필드는 '년-월-일' 의 문자열 타입이어야 합니다");
+    test("올바른 포맷의 경우 일치하는 Date 객체를 반환합니다", () => {
+        const str = "2000-04-02";
+        expect(formatDate(str)).toBe(new Date().setFullYear(2000, 4, 2));
     });
+
+    // test("올바른 포맷이 아닐 경우 에러를 반환합니다", () => {
+    //     const str = "1-2";
+    //     expect(formatDate(str)).toThrow("date 필드는 '년-월-일' 의 문자열 타입이어야 합니다");
+    // });
 });
