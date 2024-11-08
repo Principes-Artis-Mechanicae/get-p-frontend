bc2692809c5245e98edf01a0ba48cfa59072f8e7
chae-won-shin <scwcart@naver.com>
2024-11-09 01:53:47 +0900

    feat: 피플 정보 등록 시 전화번호 정규식 검사 추가

--- get-p-client/src/services/people/usePeopleInfoRegister.tsx
+++ get-p-client/src/services/people/usePeopleInfoRegister.tsx
@@ -1,8 +1,12 @@
 import { useCallback, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 import { queryClient } from "@getp/apps/config/query";
+import { REGEXP_PHONENUMBER } from "@getp/apps/constants/regex";
+
+import { useInputValidation } from "@getp/common/hooks/useInputValidation";
 
 import { authAction } from "@getp/store/slice/auth.slice";
 import { RootDispatch } from "@getp/store/store";
@@ -13,6 +17,11 @@ import { useMutation } from "@tanstack/react-query";
 
 export const usePeopleInfoRegister = () => {
     const dispatch: RootDispatch = useDispatch();
+    const {
+        value: phoneNumber,
+        isValid: isPhoneNumberValid,
+        onChange: onPhoneNumberChange,
+    } = useInputValidation(REGEXP_PHONENUMBER);
 
     const emailRef = useRef<HTMLInputElement | null>(null);
     const nicknameRef = useRef<HTMLInputElement | null>(null);
@@ -25,7 +34,7 @@ export const usePeopleInfoRegister = () => {
             peopleService.registerPeopleInfo({
                 nickname: nicknameRef.current?.value as string,
                 email: emailRef.current?.value as string,
-                phoneNumber: phoneNumberRef.current?.value as string,
+                phoneNumber: phoneNumber,
             }),
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: PEOPLE_QUERY_KEYS.PEOPLE() });
@@ -35,13 +44,19 @@ export const usePeopleInfoRegister = () => {
     });
 
     const handleNextClick = useCallback(() => {
+        if (!isPhoneNumberValid) {
+            toast.error("전화번호를 형식에 맞게 다시 입력해 주세요!");
+            return;
+        }
         mutate();
-    }, [mutate]);
+    }, [mutate, isPhoneNumberValid]);
 
     return {
         nicknameRef,
         emailRef,
         phoneNumberRef,
+        isPhoneNumberValid,
+        onPhoneNumberChange,
         handleNextClick,
     };
 };
