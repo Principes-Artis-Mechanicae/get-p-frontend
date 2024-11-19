1a4b1ba92e10a180ee34564f10e07c4bda44a220
toothlessdev <toothless042@gmail.com>
2024-11-19 18:04:52 +0900

    fix: 사용하지 않는 Import 제거

--- get-p-client/src/services/people/usePeopleInfoRegister.tsx
+++ get-p-client/src/services/people/usePeopleInfoRegister.tsx
@@ -1,7 +1,6 @@
 import { useCallback, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
 
 import { queryClient } from "@getp/apps/config/query";
 
