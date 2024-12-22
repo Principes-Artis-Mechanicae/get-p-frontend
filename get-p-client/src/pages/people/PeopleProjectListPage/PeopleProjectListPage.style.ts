4e73ae461318b9713dee35f13abda6fa0b91c32a
toothlessdev <toothless042@gmail.com>
2024-12-22 22:54:01 +0900

    feature: 지원한 페이지 퍼블리싱 및 api 연동

---
+++ get-p-client/src/pages/people/PeopleProjectListPage/PeopleProjectListPage.style.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+    width: 100%;
+    max-width: 1200px;
+
+    margin: 40px auto;
+`;
+
+export const Header = styled.div`
+    text-align: center;
+`;
+
+export const Body = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+`;
