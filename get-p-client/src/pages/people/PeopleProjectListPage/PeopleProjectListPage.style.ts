e742c5d4f5c40c848f2f7000b7dabed1dbf78171
toothlessdev <toothless042@gmail.com>
2025-01-18 17:42:21 +0900

    style: 지원한 프로젝트 grid 반응형 스타일 적용

--- get-p-client/src/pages/people/PeopleProjectListPage/PeopleProjectListPage.style.ts
+++ get-p-client/src/pages/people/PeopleProjectListPage/PeopleProjectListPage.style.ts
@@ -1,3 +1,5 @@
+import { mobile, tablet } from "@getp/styles/breakpoint";
+
 import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
@@ -9,9 +11,24 @@ export const Wrapper = styled.div`
 
 export const Header = styled.div`
     text-align: center;
+    margin: 50px 0px;
 `;
 
 export const Body = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
+    gap: 12px;
+
+    ${tablet} {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    ${mobile} {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`;
+
+export const Footer = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 20px 0px;
 `;
